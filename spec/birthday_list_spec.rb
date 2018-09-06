7c18d58a2b3383fed6a0d3743d3325866df1d10c
Andres Faraone <andresfaraone@outlook.com>
2018-09-06 14:19:12 +0100

    Fixed spec to check #add correctly

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -2,11 +2,11 @@ require 'birthday_list'
 
 describe BirthdayList do
   it "adds people's birthday" do
-    #1
+    #1 - Setup (create an object)
     birthdaylist  = BirthdayList.new
-    #2
+    #2 - Execute (make my object do something)
     birthdaylist.add("jon", "1/1/1980")
-    #3
-    expect(birthdaylist.list).to include([{name: "jon", date: "1/1/1980"}])
+    #3 - Assert (did my object do the correct thing?)
+    expect(birthdaylist.list).to include({name: "jon", date: "1/1/1980"})
   end
 end

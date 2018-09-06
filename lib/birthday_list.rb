33bf67f3472de8a87029e5b5bb6bfc55476edf7c
Antonio <antoniodaria@msn.com>
2018-09-06 15:30:31 +0100

    updating the display method using pry

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,11 +1,20 @@
+require 'pry'
 class BirthdayList
   attr_reader :list
 
   def initialize
     @list = []
   end
-  
+
   def add(name, date)
     @list << ({name: name, date: date})
   end
+
+  def display
+    @list.each do |b_hash|
+      output = "#{b_hash[:name]} #{b_hash[:date]}"
+      puts output
+    end
+  end
 end
+binding.pry
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,12 +1,30 @@
 require 'birthday_list'
 
 describe BirthdayList do
-  it "adds people's birthday" do
-    #1 - Setup (create an object)
-    birthdaylist  = BirthdayList.new
-    #2 - Execute (make my object do something)
-    birthdaylist.add("jon", "1/1/1980")
-    #3 - Assert (did my object do the correct thing?)
-    expect(birthdaylist.list).to include({name: "jon", date: "1/1/1980"})
+  describe "#add" do
+    it "adds people's birthday" do
+      #1 - Setup (create an object)
+      birthdaylist  = BirthdayList.new
+      #2 - Execute (make my object do something)
+      birthdaylist.add("jon", "1/1/1980")
+      #3 - Assert (did my object do the correct thing?)
+      expect(birthdaylist.list).to include({name: "jon", date: "1/1/1980"})
+    end
+  end
+
+  describe "#display" do
+    it "displays names and birthday in a user friendly format" do
+      #1 - Setup (create an object)
+
+      #2 - Execute (make my object do something)
+      subject.add("antonio", "6/5/1983")
+      #3 - Assert (did my object do the correct thing?)
+      expect(subject.display).to eq("antonio 6/5/1983")
+    end
+    it  "displays multiple names" do
+      subject.add("andres", "9/9/1990")
+      subject.add("antonio", "7/4/1990")
+      expect(subject.display).to eq()
+    end
   end
 end

2900e7c4db20386a329663239ff3642e2c9591fd
Antonio <antoniodaria@msn.com>
2018-09-06 14:02:30 +0100

    Created first test for the first user story

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,12 @@
+require 'birthday_list'
 
+describe BirthdayList do
+  it "adds people's birthday" do
+    #1
+    birthdaylist  = BirthdayList.new
+    #2
+    birthdaylist.add("jon", "1/1/1980")
+    #3
+    expect(birthdaylist.list).to include([{name: "jon", date: "1/1/1980"}])
+  end
+end
